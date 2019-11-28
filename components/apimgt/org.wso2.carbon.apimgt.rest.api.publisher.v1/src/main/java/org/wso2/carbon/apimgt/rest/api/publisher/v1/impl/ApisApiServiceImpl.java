fac44cdeb0f29d061bbf4ea7323b8d12a1f67add
Hiranya Abeyrathne <hiranyaa@wso2.com>
2019-11-28 21:59:14 +0530

    Adding missing default parameters to the soap api definition.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
@@ -3081,7 +3081,7 @@ public class ApisApiServiceImpl implements ApisApiService {
             //add the generated swagger definition to SOAP
             APIDefinition oasParser = new OAS2Parser();
             SwaggerData swaggerData = new SwaggerData(apiToAdd);
-            String apiDefinition = oasParser.generateAPIDefinition(swaggerData);
+            String apiDefinition = generateSOAPAPIDefinition(oasParser.generateAPIDefinition(swaggerData));
             apiProvider.saveSwaggerDefinition(apiToAdd, apiDefinition);
             APIIdentifier createdApiId = apiToAdd.getId();
             //Retrieve the newly added API to send in the response payload
@@ -3091,6 +3091,26 @@ public class ApisApiServiceImpl implements ApisApiService {
             RestApiUtil.handleInternalServerError("Error while importing WSDL to create a SOAP API", e, log);
         }
         return null;
+    }
+
+    /**
+     * Add soap parameters to the default soap api resource.
+     *
+     * @param apiDefinition The API definition string.
+     * @return Modified api definition.
+     * */
+    private String generateSOAPAPIDefinition(String apiDefinition) throws APIManagementException {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject apiJson;
+        JSONObject paths;
+        try {
+            apiJson = (JSONObject) jsonParser.parse(apiDefinition);
+            paths = (JSONObject) jsonParser.parse(RestApiPublisherUtils.getSOAPOperation());
+            apiJson.replace("paths", paths);
+            return apiJson.toJSONString();
+        } catch (ParseException e) {
+            throw new APIManagementException("Error while parsing the api definition.", e);
+        }
     }
 
     /**
